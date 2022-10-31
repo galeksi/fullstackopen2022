732ab6589c7bc5f36f1b32e26912c4b1ea512813
Aleksi Rendel <aleksi.rendel@gmail.com>
2022-10-31 16:55:16 +0200

    Finisished exercises 2.7-2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,34 +2,73 @@ import { useState } from 'react'
 
 const Person = ({ person }) =>
   <>
-    {person.name}<br />
+    {person.name} {person.number}<br />
   </>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
-  console.log(persons)
+  // console.log(persons)
 
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject = { name: newName }
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    // console.log(event.target[0].value)
+    if (checkNameExists(newName)) {
+      alert(`${newName} is already in the phonebook`)
+    }
+    else {
+      const personObject = { name: newName, number: newNumber, id: persons.length + 1 }
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
   }
 
   const handleNameChange = (event) => {
+    console.log(event.target.value)
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  } 
+
+  const handleValueChange = (event) => {
+    console.log('Filter input changed to', event.target.value)
+    setNewFilter(event.target.value)
+  }
+  
+  const checkNameExists = (newName) => {
+    console.log(newName)
+    return (
+      persons.find(person => person.name === newName)
+    )
+  }
+
+  const personsToShow = newFilter
+      ? persons.filter(person => person.name.toLowerCase().includes(newFilter.toLocaleLowerCase()))
+      : persons
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown for: <input value={newFilter} onChange={handleValueChange}/>
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input value={newName} onChange={handleNameChange} /> <br />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -37,8 +76,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <p>
-        {persons.map(person => 
-          <Person key={person.name} person={person} />
+        {personsToShow.map(person => 
+          <Person key={person.id} person={person} />
         )}
       </p>
       {/* <div>debug: {newName}</div> */}
