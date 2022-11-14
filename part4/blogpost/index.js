84c993132efce8d1655af8166a52f6646c9fe223
Aleksi Rendel <aleksi.rendel@gmail.com>
2022-11-14 16:10:39 +0200

    Finished exercises 4.1-4.2

--- part4/blogpost/index.js
+++ part4/blogpost/index.js
@@ -1,31 +1,10 @@
+const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 
-const blogsRouter = require('./controllers/blogs')
+const server = http.createServer(app)
 
-const mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl)
-    .then(() => {
-        logger.info('connected')
-    })
-    .catch((err) => logger.error('err'))
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
 })
