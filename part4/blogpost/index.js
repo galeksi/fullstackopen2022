f1d2624a3ad44073701056ebed3fbd6ed7ac7d9f
Aleksi Rendel <aleksi.rendel@gmail.com>
2022-11-14 15:46:27 +0200

    Added model and controller

--- part4/blogpost/index.js
+++ part4/blogpost/index.js
@@ -6,16 +6,19 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', blogSchema)
+// const blogSchema = new mongoose.Schema({
+//     title: String,
+//     author: String,
+//     url: String,
+//     likes: Number
+// })
+
+// const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
+
 mongoose.connect(mongoUrl)
     .then(() => {
         logger.info('connected')
@@ -25,25 +28,8 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-// const PORT = 3003
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
 })
