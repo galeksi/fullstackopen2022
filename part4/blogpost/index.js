e661ac3cc9c8585047a3df24ba230791a9fb1415
Aleksi Rendel <aleksi.rendel@gmail.com>
2022-11-14 15:57:37 +0200

    Added utils middleware

--- part4/blogpost/index.js
+++ part4/blogpost/index.js
@@ -1,21 +1,13 @@
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
-
-// const blogSchema = new mongoose.Schema({
-//     title: String,
-//     author: String,
-//     url: String,
-//     likes: Number
-// })
-
-// const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 
@@ -27,8 +19,12 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
