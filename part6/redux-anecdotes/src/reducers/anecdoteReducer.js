08c81c4664a5b3bbbb3cb6d20423c7bd61c879b3
Aleksi Rendel <aleksi.rendel@gmail.com>
2022-12-12 15:10:39 +0200

    Finished exercises 6.3-6.8

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -17,13 +17,48 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+const sortAnecdotes = (anecdotes) => anecdotes.sort((a, b) => b.votes - a.votes)
+
+const initialState = sortAnecdotes(anecdotesAtStart.map(asObject))
 
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  switch(action.type) {
+    case 'VOTE': {
+      const id = action.data.id
+      const anecdoteToVote = state.find(a => a.id === id)
+      const votedAnecdote = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes += 1
+      }
+      return sortAnecdotes(state.map(anecdote =>
+        anecdote.id !== id ? anecdote : votedAnecdote
+      ))
+    }
+    case 'NEW_ANECDOTE':
+      return state.concat(action.data)
+    default:
+      return state
+  }
+}
 
-  return state
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: {
+      content,
+      id: getId(),
+      votes: 0
+    }
+  }
 }
 
 export default reducer
