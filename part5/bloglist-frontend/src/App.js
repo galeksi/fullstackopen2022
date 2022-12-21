d4f3b76cb85eef05a8cb17b9a662d14fda17c53b
Aleksi Rendel <aleksi.rendel@gmail.com>
2022-12-21 16:43:24 +0200

    Finisched exercises 7.14-7.17

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,26 +1,45 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
-import Notification from './components/Notification'
-import { setNotification } from './reducers/notificationReducer'
-import Togglable from './components/Togglable'
-import BlogForm from './components/BlogForm'
 import { useDispatch, useSelector } from 'react-redux'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams,
+  Link,
+} from 'react-router-dom'
+// import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
+import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import blogService from './services/blogs'
+import userService from './services/users'
+import { setNotification } from './reducers/notificationReducer'
+import { loginUser, setUser, clearUser } from './reducers/userReducer'
 import {
   initializeBlogs,
   createBlog,
   likeBlog,
   destroyBlog,
 } from './reducers/blogReducer'
-import { loginUser, setUser, clearUser } from './reducers/userReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [users, setUsers] = useState([])
+
+  const initializeUsers = async () => {
+    const initialUsers = await userService.getAll()
+    // console.log(initialUsers)
+    setUsers(initialUsers)
+  }
 
   useEffect(() => {
     dispatch(initializeBlogs())
+  }, [])
+
+  useEffect(() => {
+    initializeUsers()
   }, [])
 
   useEffect(() => {
@@ -88,44 +107,147 @@ const App = () => {
     dispatch(likeBlog(id, updatedLikes))
   }
 
-  const deleteBlog = async (id) => {
+  const deleteBlog = (id) => {
     const blogToDelete = blogs.find((b) => b.id === id)
     if (window.confirm(`Delete ${blogToDelete.title}`)) {
       dispatch(destroyBlog(id))
     }
   }
 
-  return (
-    <div>
-      <h1>Bloglist</h1>
-      <Notification
-        message={notification.message}
-        messageType={notification.type}
-      />
-      {user === null ? (
-        loginForm()
-      ) : (
+  const BlogList = ({ blogs }) => {
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5,
+    }
+    return (
+      <div>
+        <h2 className="bloglist">blogs</h2>
+        {blogs.map((blog) => (
+          <div key={blog.id} style={blogStyle}>
+            <Link to={`/blogs/${blog.id}`}>
+              {blog.title} {blog.author}
+            </Link>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  const BlogView = ({ blogs }) => {
+    const id = useParams().id
+    const blog = blogs.find((u) => u.id === id)
+    if (!blog) {
+      return null
+    }
+
+    return (
+      <div>
+        <h1>{blog.title}</h1>
+        <a href={blog.url}>{blog.url}</a>
         <div>
-          <p>
-            {user.name} logged-in&nbsp;
-            <button onClick={handleLogout}>logout</button>
-          </p>
-          <Togglable buttonLabel={'Add blog'} buttonLabelBack={'cancel'}>
-            <BlogForm createBlog={addBlog} />
-          </Togglable>
+          {blog.likes}&nbsp;likes&nbsp;
+          <button onClick={() => updateBlogLikes(blog.id)}>like</button>
+          <div>added by: {blog.author}</div>
+          {user && blog.user.id === user.id ? (
+            <button onClick={() => deleteBlog(blog.id)}>Delete</button>
+          ) : null}
         </div>
-      )}
-      <h2 className="bloglist">blogs</h2>
-      {blogs.map((blog) => (
-        <Blog
-          key={blog.id}
-          blog={blog}
-          user={user}
-          updateBlogLikes={updateBlogLikes}
-          deleteBlog={deleteBlog}
+      </div>
+    )
+  }
+
+  const Users = ({ users }) => {
+    // console.log(users)
+    return (
+      <div>
+        <h2 className="users">Users</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td>User name</td>
+              <td>blogs created</td>
+            </tr>
+            {users.map((user) => (
+              <tr key={user.username}>
+                <td>
+                  <Link to={`/users/${user.id}`}>{user.name}</Link>
+                </td>
+                <td style={{ textAlign: 'right' }}>{user.blogs.length}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+  const UserView = ({ users }) => {
+    const id = useParams().id
+    const user = users.find((u) => u.id === id)
+
+    if (!user) {
+      return null
+    }
+    return (
+      <div>
+        <h2>{user.name}</h2>
+        <h3>added blogs</h3>
+        <ul>
+          {user.blogs.map((blog) => (
+            <li key={blog.id}>{blog.title}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  const padding = {
+    padding: 5,
+  }
+
+  return (
+    <Router>
+      <div>
+        <div>
+          <Link style={padding} to="/">
+            blogs
+          </Link>
+          <Link style={padding} to="/users">
+            users
+          </Link>
+          {user ? (
+            <>
+              <em>{user.name} logged-in&nbsp;</em>
+              <button onClick={handleLogout}>logout</button>
+            </>
+          ) : null}
+        </div>
+        <Notification
+          message={notification.message}
+          messageType={notification.type}
         />
-      ))}
-    </div>
+        <h1>Bloglist</h1>
+        {user === null ? (
+          loginForm()
+        ) : (
+          <div>
+            <Togglable buttonLabel={'Add blog'} buttonLabelBack={'cancel'}>
+              <BlogForm createBlog={addBlog} />
+            </Togglable>
+          </div>
+        )}
+
+        <Routes>
+          <Route path="/" element={<BlogList blogs={blogs} />} />
+          <Route path="/blogs/:id" element={<BlogView blogs={blogs} />} />
+          <Route path="/users" element={<Users users={users} />} />
+          <Route path="/users/:id" element={<UserView users={users} />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,59 +1,64 @@
-import { useState } from 'react'
-import PropTypes from 'prop-types'
+// ### NOT IN USE
 
-const Blog = ({ blog, user, updateBlogLikes, deleteBlog }) => {
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5,
-  }
+// import { useState } from 'react'
+// import PropTypes from 'prop-types'
+// import { Link } from 'react-router-dom'
 
-  const [visible, setVisible] = useState(true)
-  const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
-  const toggleVisibility = () => {
-    setVisible(!visible)
-  }
+// const Blog = ({ blog, user, updateBlogLikes, deleteBlog }) => {
+//   const blogStyle = {
+//     paddingTop: 10,
+//     paddingLeft: 2,
+//     border: 'solid',
+//     borderWidth: 1,
+//     marginBottom: 5,
+//   }
 
-  return (
-    <div className="blog">
-      <div
-        className="blogShort"
-        style={Object.assign(showWhenVisible, blogStyle)}
-      >
-        {blog.title} {blog.author}
-        <button onClick={toggleVisibility}>view</button>
-      </div>
-      <div
-        className="blogLong"
-        style={Object.assign(hideWhenVisible, blogStyle)}
-      >
-        {blog.title} {blog.author}
-        <button onClick={toggleVisibility}>hide</button>
-        <br />
-        {blog.url}
-        <br />
-        likes&nbsp;{blog.likes}&nbsp;
-        <button onClick={() => updateBlogLikes(blog.id)}>like</button>
-        <br />
-        {blog.user.name}
-        <br />
-        {/* {blog.user.id}<br />
-        {user.id}<br /> */}
-        {user && blog.user.id === user.id ? (
-          <button onClick={() => deleteBlog(blog.id)}>Delete</button>
-        ) : null}
-      </div>
-    </div>
-  )
-}
+//   const [visible, setVisible] = useState(true)
+//   const hideWhenVisible = { display: visible ? 'none' : '' }
+//   const showWhenVisible = { display: visible ? '' : 'none' }
+//   const toggleVisibility = () => {
+//     setVisible(!visible)
+//   }
 
-Blog.propTypes = {
-  blog: PropTypes.object.isRequired,
-  updateBlogLikes: PropTypes.func.isRequired,
-  deleteBlog: PropTypes.func.isRequired,
-}
+//   return (
+//     <div className="blog">
+//       <div
+//         className="blogShort"
+//         style={Object.assign(showWhenVisible, blogStyle)}
+//       >
+//         <Link to={`/blogs/${blog.id}`}>
+//           {blog.title} {blog.author}
+//         </Link>
+//         <button onClick={toggleVisibility}>view</button>
+//       </div>
+//       <div
+//         className="blogLong"
+//         style={Object.assign(hideWhenVisible, blogStyle)}
+//       >
+//         {blog.title} {blog.author}
+//         <button onClick={toggleVisibility}>hide</button>
+//         <br />
+//         {blog.url}
+//         <br />
+//         likes&nbsp;{blog.likes}&nbsp;
+//         <button onClick={() => updateBlogLikes(blog.id)}>like</button>
+//         <br />
+//         {blog.user.name}
+//         <br />
+//         {/* {blog.user.id}<br />
+//         {user.id}<br /> */}
+//         {user && blog.user.id === user.id ? (
+//           <button onClick={() => deleteBlog(blog.id)}>Delete</button>
+//         ) : null}
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Blog
+// Blog.propTypes = {
+//   blog: PropTypes.object.isRequired,
+//   updateBlogLikes: PropTypes.func.isRequired,
+//   deleteBlog: PropTypes.func.isRequired,
+// }
+
+// export default Blog
