ba7b81d0b1aecda76b3ee0942fe35ebcf18c1d3e
Aleksi Rendel <aleksi.rendel@gmail.com>
2022-12-22 14:50:52 +0200

    Added changes for comments in part7

--- part4/blogpost/controllers/blogs.js
+++ part4/blogpost/controllers/blogs.js
@@ -1,71 +1,63 @@
-const blogsRouter = require('express').Router()
-const jwt = require('jsonwebtoken')
-const Blog = require('../models/blog')
-const User = require('../models/user')
+const blogsRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
-blogsRouter.get('/', async (request, response) => {
+blogsRouter.get("/", async (request, response) => {
   // await Blog.deleteMany({})
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  response.json(blogs)
-})
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(blogs);
+});
 
-blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
   if (blog) {
-    response.json(blog)
+    response.json(blog);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-blogsRouter.post('/', async (request, response) => {
-  const blog = request.body
-  if (!blog.title || !blog.url) return response.status(400).end()
+blogsRouter.post("/", async (request, response) => {
+  const blog = request.body;
+  if (!blog.title || !blog.url) return response.status(400).end();
 
-  const user = request.user
+  const user = request.user;
 
   const blogToSave = new Blog({
     title: blog.title,
     author: blog.author,
     url: blog.url,
     likes: blog.likes,
-    user: user.id
-  })
+    comments: blog.comments,
+    user: user.id,
+  });
 
-  const savedBlog = await blogToSave.save()
-  user.blogs = user.blogs.concat(savedBlog.id)
-  await user.save()
+  const savedBlog = await blogToSave.save();
+  user.blogs = user.blogs.concat(savedBlog.id);
+  await user.save();
 
-  response.status(201).json(savedBlog)
-})
+  response.status(201).json(savedBlog);
+});
 
-blogsRouter.put('/:id', async (request, response) => {
-  const blog = request.body
+blogsRouter.put("/:id", async (request, response) => {
+  const blog = request.body;
 
-  // const blog = {
-  //   title: body.title,
-  //   author: body.author,
-  //   url: body.url,
-  //   likes: body.likes,
-  // }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
 
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  response.json(updatedBlog)
-})
-
-blogsRouter.delete('/:id', async (request, response) => {
-  // const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  // if (!decodedToken.id) {
-  //   return response.status(401).json({ error: 'token missing or invalid' })
-  // }
-  const  user = request.user
-  const blog = await Blog.findById(request.params.id)
+blogsRouter.delete("/:id", async (request, response) => {
+  const user = request.user;
+  const blog = await Blog.findById(request.params.id);
   if (!(blog.user.toString() === user.id)) {
-    return response.status(401).json({ error: 'invalid user for deletion' })
+    return response.status(401).json({ error: "invalid user for deletion" });
   }
-  
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
 
-module.exports = blogsRouter
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+module.exports = blogsRouter;
