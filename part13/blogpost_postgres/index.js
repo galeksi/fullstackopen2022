ad514613212634b407bf17a8177eb4f0776c6e8f
Aleksi Rendel <aleksi.rendel@gmail.com>
2023-04-12 16:09:53 +0300

    Finished exercise 13.3-13.4

--- part13/blogpost_postgres/index.js
+++ part13/blogpost_postgres/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Sequelize, Model, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 // const sequelize = new Sequelize(process.env.DATABASE_URL, {
 //   dialectOptions: {
@@ -47,18 +48,38 @@ Blog.init(
   }
 );
 
+Blog.sync();
+
 app.get("/api/blogs", async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
 });
 
 app.post("/api/blogs", async (req, res) => {
-  console.log(req.body);
   try {
     const blog = await Blog.create(req.body);
     return res.json(blog);
   } catch (error) {
     return res.status(400).json({ error });
+  }
+});
+
+app.get("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.destroy();
+    res.status(204).end();
+  } else {
+    res.status(400).json({ error: "invalid user for deletion" });
   }
 });
 
